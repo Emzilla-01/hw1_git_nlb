1f027e9cf23a88170445ad3f95c83a3feab9ef68
Emzilla-01 <Emzilla-01@github.com>
2020-12-07 01:04:19 -0500

    rebase

--- my_code/regex_like_.sql
+++
@@ -1,6 +0,0 @@
-# Email id columns should contain a @ and . (i.e. Dot)
-#select LOCATE('@', 'apple@fruit.com') from dual;
-#like "_%@_%._%" # email pattern
-# % # _%@_%._%
-# + 
-#check <-- constraint keyword
